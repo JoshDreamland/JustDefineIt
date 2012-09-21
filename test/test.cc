447e20ee430379e8d3eabd6feefa6fa89ef0f923
Josh @ Dreamland <JoshV@zoominternet.net>
2012-09-21 11:57:54 -0400

    Fixes any and all memory leaks and segmentation faults when parsing every C++ standard library header I could name.

--- test/test.cc
+++ test/test.cc
@@ -1,1 +1,69 @@
-#include "SHELLmain.cpp"
+/*
+typedef int x;
+template<typename _Tp> struct _Array { _Array(x); };
+#pragma DEBUG_ENTRY_POINT
+template<typename _Tp> inline _Array<_Tp>::_Array(x __n) { }
+*/
+
+/*
+template<int x> class b;
+template<int x> class
+#pragma DEBUG_ENTRY_POINT
+b: syntax_error {};
+*/
+
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <hash_map>
+#include <hash_set>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <iso646.h>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <utility>
+#include <vector>
+#include <cmath>
+#include <valarray>
+
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <GL/gl.h>
